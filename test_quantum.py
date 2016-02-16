bb18d2d0b76f05d3c708157347d27a83b43a9d9e
rahul080327 <rahul080327@gmail.com>
2016-02-16 22:38:22 +0530

    Rahul | Added automated py tests for the jsons

---
+++ test_quantum.py
@@ -0,0 +1,18 @@
+import unittest
+from subprocess import check_output
+
+
+def run(json):
+    return check_output(["./quantum", "test_jsons/%s" % json]).strip()
+
+
+class Quantum_Test(unittest.TestCase):
+
+    def test_print_int(self):
+        self.assertEqual("345", run("print.json"))
+
+    def test_add_floats(self):
+        self.assertEqual("44.21", run("add.json"))
+
+if __name__ == '__main__':
+    unittest.main()
